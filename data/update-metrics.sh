dc5cbb5f32c8f1a096be5a7d44692593de45e8ee
cytopia <cytopia@everythingcli.org>
2019-08-17 17:23:50 +0200

    Force overwriting new metrics file

--- data/update-metrics.sh
+++ data/update-metrics.sh
@@ -54,7 +54,7 @@ while true; do
 	START=$(date +%s)
 	if /usr/bin/aws-ec2-sg-exporter > /var/www/index.html.new; then
 		END=$(date +%s)
-		mv /var/www/index.html.new /var/www/index.html
+		mv -f /var/www/index.html.new /var/www/index.html
 		printf "[OK]  %s (%s): %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "${NAME}" "Metrics updated in $(( ${END} - ${START} )) sec"
 	else
 		END=$(date +%s)
